5635a62030b06e34aedee1a3df9db14249b0e3ef
shwet <shwet@ShwetaPunekar.mshome.net>
2024-06-26 12:20:16 +0530

    Added the product page to Object repository utility

---
+++ ComcastCRMGUIFrameworkDemo/src/main/java/com/comcast/crm/objectrepository/ProductPage.java
@@ -0,0 +1,24 @@
+package com.comcast.crm.objectrepository;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProductPage 
+{
+	WebDriver driver;
+	public ProductPage(WebDriver driver)
+	{
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath = "//img[@src='themes/softed/images/btnL3Add.gif']")
+	private WebElement createProductbtn;
+	
+	public WebElement getProductbtn()
+	{
+		return createProductbtn;
+	}
+}
